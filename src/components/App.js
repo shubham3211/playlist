459a9303ea99f8f6fb574158fa685f9edeb30990
shubham <kumarsinghshubham32111@gmail.com>
2019-08-10 08:16:07 +0530

    netify added

--- src/components/App.js
+++ src/components/App.js
@@ -21,7 +21,7 @@ class App extends React.Component {
 	  }
 
 	  if(!hashParams.access_token) {
-	    window.location.href = 'https://accounts.spotify.com/authorize?client_id=9bc9cd6f229443d5b183b2111b3901b1&scope=playlist-read-private%20playlist-read-collaborative%20playlist-modify-public%20user-read-recently-played%20playlist-modify-private%20ugc-image-upload%20user-follow-modify%20user-follow-read%20user-library-read%20user-library-modify%20user-read-private%20user-read-email%20user-top-read%20user-read-playback-state&response_type=token&redirect_uri=http://localhost:3000/callback';
+	    window.location.href = 'https://accounts.spotify.com/authorize?client_id=9bc9cd6f229443d5b183b2111b3901b1&scope=playlist-read-private%20playlist-read-collaborative%20playlist-modify-public%20user-read-recently-played%20playlist-modify-private%20ugc-image-upload%20user-follow-modify%20user-follow-read%20user-library-read%20user-library-modify%20user-read-private%20user-read-email%20user-top-read%20user-read-playback-state&response_type=token&redirect_uri=https://frosty-perlman-bd4e0e.netlify.com/callback';
 	  } else {
       Spotify.setAccessToken(hashParams.access_token);
       this.setState({hashParams: hashParams})
